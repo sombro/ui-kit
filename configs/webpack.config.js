c9aa69ee682aee80fcbf300bb3c465233d7e5aa5
nbuevich <nikitabuevich@gmail.com>
2016-04-05 17:20:41 +0300

    ButtonControl

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -1,31 +1,52 @@
 /* eslint-disable no-var */
 var webpack = require('webpack');
 var path = require('path');
+var autoprefixer = require('autoprefixer');
 
 module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:5000',
-    'webpack/hot/dev-server',
-    './client/'
-  ],
-  output: {
-    path: '/build/',
-    filename: 'index.js'
-  },
-  resolve: {
-  },
-  devtool: '#inline-source-map',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        loaders: ['babel'],
-      }
-    ]
-  }
+	entry: [
+		'webpack-dev-server/client?http://localhost:5000',
+		'webpack/hot/dev-server',
+		'./client/'
+	],
+	output: {
+		path: '/build/',
+		filename: 'index.js'
+	},
+	resolve: {
+		root: [
+			path.resolve('./client/'),
+		],
+	},
+	devtool: '#inline-source-map',
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	],
+	module: {
+		loaders: [
+			{
+				test: /\.js?$/,
+				exclude: /node_modules/,
+				loaders: ['babel'],
+			},
+			{
+                test: /\.json$/,
+                loaders: ['json'],
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css', 'postcss'],
+            },
+            {
+                test: /\.scss/,
+                loaders: ['style', 'css', 'postcss', 'resolve-url', 'sass'],
+            },
+		]
+	},
+	postcss: function () {
+		return [
+			autoprefixer({ browsers: ['last 2 versions', 'ie 9'] }),
+		]
+	},
 };
--- server/server.js
+++ server/server.js
@@ -3,7 +3,13 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('../configs/webpack.config.js');
 
-new WebpackDevServer(webpack(config), {}).listen(5000, 'localhost', function (err) {
+new WebpackDevServer(webpack(config), {
+	quiet: false,
+	noInfo: true,
+	stats: {
+		colors: true
+	},
+}).listen(5000, 'localhost', function (err) {
 	if (err) {
 	  console.log(err);
 	}
