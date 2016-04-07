1e0ba377848063e1c1dca5a7974c2dc4aac80089
nbuevich <nikitabuevich@gmail.com>
2016-04-07 15:39:01 +0300

    some UIKit-page changes and Checkbox and Switch disabled styles

--- client/index.js
+++ client/index.js
@@ -11,31 +11,31 @@ ReactDOM.render(
 		<fieldset className="UIKit__buttons">
 			<legend>Buttons</legend>
 			<h3>Default</h3>
-			<ButtonControl view="crystal">Кнопочка crystal</ButtonControl>
-			<ButtonControl view="sand">Кнопочка sand</ButtonControl>
-			<ButtonControl view="sky">Кнопочка sky</ButtonControl>
-			<ButtonControl view="forest">Кнопочка forest</ButtonControl>
-			<ButtonControl view="stone">Кнопочка stone</ButtonControl>
-			<ButtonControl view="blackLink">Кнопочка blackLink</ButtonControl>
+			<ButtonControl view="crystal">Button crystal</ButtonControl>
+			<ButtonControl view="sand">Button sand</ButtonControl>
+			<ButtonControl view="sky">Button sky</ButtonControl>
+			<ButtonControl view="forest">Button forest</ButtonControl>
+			<ButtonControl view="stone">Button stone</ButtonControl>
+			<ButtonControl view="blackLink">Button blackLink</ButtonControl>
 			<h3>With shadow</h3>
-			<ButtonControl view="crystal" shadow={true}>Кнопочка crystal</ButtonControl>
-			<ButtonControl view="sand" shadow={true}>Кнопочка sand</ButtonControl>
-			<ButtonControl view="sky" shadow={true}>Кнопочка sky</ButtonControl>
-			<ButtonControl view="forest" shadow={true}>Кнопочка forest</ButtonControl>
-			<ButtonControl view="stone" shadow={true}>Кнопочка stone</ButtonControl>
-			<ButtonControl view="blackLink" shadow={true}>Кнопочка blackLink</ButtonControl>
+			<ButtonControl view="crystal" shadow={true}>Button crystal</ButtonControl>
+			<ButtonControl view="sand" shadow={true}>Button sand</ButtonControl>
+			<ButtonControl view="sky" shadow={true}>Button sky</ButtonControl>
+			<ButtonControl view="forest" shadow={true}>Button forest</ButtonControl>
+			<ButtonControl view="stone" shadow={true}>Button stone</ButtonControl>
+			<ButtonControl view="blackLink" shadow={true}>Button blackLink</ButtonControl>
 			<h3>Sizes</h3>
-			<ButtonControl view="sand" size="s">Кнопочка sand S</ButtonControl>
-			<ButtonControl view="sand" size="m">Кнопочка sand M</ButtonControl>
+			<ButtonControl view="sand" size="s">Button sand S</ButtonControl>
+			<ButtonControl view="sand" size="m">Button sand M</ButtonControl>
 			<h3>Disabled</h3>
-			<ButtonControl view="crystal" disabled>Кнопочка crystal</ButtonControl>
-			<ButtonControl view="sand" disabled>Кнопочка sand</ButtonControl>
-			<ButtonControl view="sky" disabled>Кнопочка sky</ButtonControl>
-			<ButtonControl view="forest" disabled>Кнопочка forest</ButtonControl>
-			<ButtonControl view="stone" disabled>Кнопочка stone</ButtonControl>
-			<ButtonControl view="blackLink" disabled>Кнопочка blackLink</ButtonControl>
+			<ButtonControl view="crystal" disabled>Button crystal</ButtonControl>
+			<ButtonControl view="sand" disabled>Button sand</ButtonControl>
+			<ButtonControl view="sky" disabled>Button sky</ButtonControl>
+			<ButtonControl view="forest" disabled>Button forest</ButtonControl>
+			<ButtonControl view="stone" disabled>Button stone</ButtonControl>
+			<ButtonControl view="blackLink" disabled>Button blackLink</ButtonControl>
 			<h3>With link</h3>
-			<ButtonControl view="sand" href="#">Кнопочка sand</ButtonControl>
+			<ButtonControl view="sand" href="#">Button sand</ButtonControl>
 		</fieldset>
 		<fieldset className="UIKit__checkboxes">
 			<legend>Checkbox</legend>
@@ -43,12 +43,12 @@ ReactDOM.render(
 			<CheckboxControl/>
 			<CheckboxControl value={true}/>
 			<CheckboxControl label="Checkbox label"/>
-			<CheckboxControl label="Checkbox" subLabel="Checkbox sublabel"/>
+			<CheckboxControl label="Checkbox label" subLabel="Checkbox sublabel"/>
 			<h3>Disabled</h3>
 			<CheckboxControl disabled/>
 			<CheckboxControl value={true} disabled/>
 			<CheckboxControl label="Checkbox label" disabled/>
-			<CheckboxControl label="Checkbox" subLabel="Checkbox sublabel" disabled/>
+			<CheckboxControl label="Checkbox label" subLabel="Checkbox sublabel" disabled/>
 		</fieldset>
 		<fieldset className="UIKit__switches">
 			<legend>Switch</legend>
